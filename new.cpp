477b4d0ef5b0dc39c93cb043d6a4ad6c4e6b8638
sumit mahant <sumitmahant31@gmail.com>
2026-09-03 00:09:33 +0530

    furr

--- new.cpp
+++ new.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-int num=15;
- num=20;
+ const int num=15;//we can't change this value like num=30!
+ int num2=20;
 cout<<"hello world"<<"\n";
 cout<<"the number is=:"<<num<<endl;
+cout<<"the 2nd is number is=:"<<num2<<endl;
 
 
 
