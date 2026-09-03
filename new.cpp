baa6c823fead10acb495a2ab3532662415b42ec2
sumit mahant <sumitmahant31@gmail.com>
2026-09-03 15:35:04 +0530

    day2

--- new.cpp
+++ new.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main(){
  const int num=15;//we can't change this value like num=30!
  int num2=20;
+ long l=1231231231;
+ float n=12.66;
+ double d=1231.43535;
+ bool b=true;
 cout<<"hello world"<<"\n";
 cout<<"the number is=:"<<num<<endl;
 cout<<"the 2nd is number is=:"<<num2<<endl;
