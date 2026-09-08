699d3d76e4bf9907c03a7d2bf3bfe8259b1b994c
sumit mahant <sumitmahant31@gmail.com>
2026-09-08 14:51:25 +0530

    new

--- new.cpp
+++ new.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 int main(){
- const int num=15;//we can't change this value like num=30!
- int num2=20;
- long l=1231231231;
- float n=12.66;
- double d=1231.43535;
- bool b=true;
- char c='*';
-cout<<"hello world"<<"\n";
-cout<<"the number is=:"<<num<<endl;
-cout<<"the 2nd is number is=:"<<num2<<endl;
-cout<<"long=:"<<d<<endl;
-cout<<"long=:"<<b<<endl;
-cout<<"long=:"<<c<<endl;
-cout<<"long=:"<<l<<endl;
-cout<<sizeof(d);
+//  const int num=15;//we can't change this value like num=30!
+//  int num2=20;
+//  long l=1231231231;
+//  float n=12.66;
+//  double d=1231.43535;
+//  bool b=true;
+//  char c='*';
+// cout<<"hello world"<<"\n";
+// cout<<"the number is=:"<<num<<endl;
+// cout<<"the 2nd is number is=:"<<num2<<endl;
+// cout<<"long=:"<<d<<endl;
+// cout<<"long=:"<<b<<endl;
+// cout<<"long=:"<<c<<endl;
+// cout<<"long=:"<<l<<endl;
+// cout<<sizeof(d);
+double a=10.40;
+int d=a;
+  cout <<d<<"\n";
+
 
 
 
