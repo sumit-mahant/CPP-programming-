b8bbe5fa38a2c500e03e397e92aa44b73126b783
sumit mahant <sumitmahant31@gmail.com>
2026-09-09 22:03:37 +0530

    jnk

--- new.cpp
+++ new.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-//  const int num=15;//we can't change this value like num=30!
-//  int num2=20;
-//  long l=1231231231;
-//  float n=12.66;
-//  double d=1231.43535;
-//  bool b=true;
-//  char c='*';
-// cout<<"hello world"<<"\n";
-// cout<<"the number is=:"<<num<<endl;
-// cout<<"the 2nd is number is=:"<<num2<<endl;
-// cout<<"long=:"<<d<<endl;
-// cout<<"long=:"<<b<<endl;
-// cout<<"long=:"<<c<<endl;
-// cout<<"long=:"<<l<<endl;
-// cout<<sizeof(d);
-double a=10.40;
-int d=a;
-  cout <<d<<"\n";
-
-
+ //typecasting!!⭐
+  
+//  int d=a;
+float a;
+ cout<<a<<endl;// it give us garvage value !!
+//  int d=(int)a;
 
 
 
