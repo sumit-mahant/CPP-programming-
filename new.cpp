b9c8ce9669b53b49c69fba0ac622853432f0422c
sumit mahant <sumitmahant31@gmail.com>
2026-09-10 00:43:52 +0530

    gfhgf

--- new.cpp
+++ new.cpp
@@ -4,32 +4,33 @@ int main(){
  //typecasting!!⭐
   
 //  int d=a;
-float a;
+// float a;
 //  cout<<a<<endl;// it give us garvage value !!
-//  int d=(int)a;
-int x=1;
+// //  int d=(int)a;
+// int x=1;
 // double ans=(double)x/y;//type casting !!
 // cout<<ans<<endl;
 // char ch='a';
  
 // cout<<ch+1<<endl;
-cout<<x<<"\n";
-cout<<x<<"\n";
-cout<<x<<"\n";
-cout<<--x<<"\n";
-// cout<<x-y<<"\n";
-// cout<<x*y<<"\n";
-// cout<<x/y<<"\n";
-// cout<<x%y<<"\n";
-cout<<((20>30)&&(40>20))<<"\n";
-cout<<((20<30)||(40<20))<<"\n";
-cout<<((20<30)&&(40>20))<<"\n";
-int val;
+// cout<<x<<"\n";
+// cout<<x<<"\n";
+// cout<<x<<"\n";
+// cout<<--x<<"\n";
+// // cout<<x-y<<"\n";
+// // cout<<x*y<<"\n";
+// // cout<<x/y<<"\n";
+// // cout<<x%y<<"\n";
+// cout<<((20>30)&&(40>20))<<"\n";
+// cout<<((20<30)||(40<20))<<"\n";
+// cout<<((20<30)&&(40>20))<<"\n";
+// int val;
 
-cout<<"enter a number=:";
-cin>>val;
-cout<<val<<endl;
-
+// cout<<"enter a number=:";
+// cin>>val;
+// cout<<val<<endl;
+int a,b;
+cout<<"Enter two numbers-:";
 
     return 0;
 }
