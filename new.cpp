1be8860ec1327a2c920d6c70c40d13256e16852e
sumit mahant <sumitmahant31@gmail.com>
2026-09-09 22:08:59 +0530

    dkfl

--- new.cpp
+++ new.cpp
@@ -5,9 +5,11 @@ int main(){
   
 //  int d=a;
 float a;
- cout<<a<<endl;// it give us garvage value !!
+//  cout<<a<<endl;// it give us garvage value !!
 //  int d=(int)a;
-
+int x=26,y=5;
+double ans=(double)x/y;//type casting !!
+cout<<ans<<endl;
 
 
 
