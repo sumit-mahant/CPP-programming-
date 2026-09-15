9b33130a8165fdebcb6c8fd6f87aea38f87817b9
sumit mahant <sumitmahant31@gmail.com>
2026-09-15 20:19:02 +0530

    pro

--- new.cpp
+++ new.cpp
@@ -40,8 +40,8 @@ string name;
 int age;
 // getline(cin,name);
 cout<<"Enter your name-:";
-//  getline(cin,name);//from this you can use space //for long string 
-cin>>name;//you can not use space 
+ getline(cin,name);//from this you can use space //for long string 
+// cin>>name;//you can not use space 
 cout<<"Enter your age-:";
 cin>>age;
 cout<<"your name is "<<name<<" & your age is "<<age<<endl;
