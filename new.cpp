949d644fcb12eb49a47f3a9c67e6590407ee9686
sumit mahant <sumitmahant31@gmail.com>
2026-09-17 21:05:29 +0530

    jhgkh

--- new.cpp
+++ new.cpp
@@ -158,8 +158,13 @@ int main(){
 // default:
 //     break;
 // }
-
-
+char ch;
+cout<<"enter a character";
+cin>>ch;
+if(ch =='a'||ch=='u'|| ch =='i'||ch=='o'||ch=='e')
+  cout<<"the char is vowal";
+else
+ cout<<"the char is consonent";
 
 
 
