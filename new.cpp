b9cd1e6f6795120435e2d9e0940373524fa68d52
sumit mahant <sumitmahant31@gmail.com>
2026-09-17 23:47:23 +0530

    normal

--- new.cpp
+++ new.cpp
@@ -166,27 +166,28 @@ int main(){
 // else
 //  cout<<"the char is consonent";
 //now same  question by Switch case!!
-char ch='a';
-switch (ch){
+// char ch='t';
+// switch (ch){
 
-    case 'a':
-    cout<<"the char is vowal";
-
-    case 'i':
-    cout<<"the char is vowal";
-    case 'e':
-    cout<<"the char is vowal";
-    case 'o':
-    cout<<"the char is vowal";
-    case 'u':
-    cout<<"the char is vowal";
-    
-
-
-
+//     case 'a': cout<<"the char is vowal";
+   
+//           break;
+//     case 'i':
+//     cout<<"the char is vowal";
+//     case 'e':
+//     cout<<"the char is vowal";
+//     case 'o':
+//     cout<<"the char is vowal";
+//     case 'u':
+//     cout<<"the char is vowal";
+//     default: cout<<"the char is consonent";
+// }   
 
 
 
-}
+
+
+
+
 
 }
