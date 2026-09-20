5d7c6320741ecfb032b411dbf051a62945297306
sumit mahant <sumitmahant31@gmail.com>
2026-09-21 00:11:51 +0530

    kfjlsfl

--- new.cpp
+++ new.cpp
@@ -257,15 +257,19 @@ int main(){
 //     printHello();//function calling !!!!
 //    int totel= sum(5,10);//arguments!!!
 //    cout<<totel<<endl;
-int arr[5]={10,20,30,40,50};
+//int arr[5]={10,20,30,40,50};
 //  arr[0]=10;
 //  arr[1]=20;
 //  arr[2]=30;
 //  arr[3]=40;
 //  arr[4]=50;
- for(int i=0;i<=4;i++){ 
-    cout<<arr[i]<<endl;
- }
+//  for(int i=0;i<=4;i++){ 
+//     cout<<arr[i]<<endl;
+//  }
+
+
+
+
 
 
 }
