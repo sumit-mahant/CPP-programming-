fa0098d812f57de206353570d1cc41172fdfd4f3
sumit mahant <sumitmahant31@gmail.com>
2026-09-21 14:53:55 +0530

    kjadjld

--- new.cpp
+++ new.cpp
@@ -285,11 +285,11 @@ int main(){
 //     sum=sum+arr[i];
 // }cout<<"sum of the array="<<sum;
 int arr[]={11,42,93,34,55,76};//sum of the array
- int size=0;int =0;
+ int size=0;int max=arr[0];
  size=sizeof(arr)/sizeof(arr[0]);// size of an array👌👌👌👌
  cout<<size<<endl;
  for(int i=0;i<size;i++){
-     sum=sum+arr[i];
+     
  }cout<<"sum of the array="<<sum;
 
 
